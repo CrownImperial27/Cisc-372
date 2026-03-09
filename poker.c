c1a02fa6c9d3a25cc225cd5baad18ebe7716dc5e
Arron <arronye@udel,edu>
2026-03-09 17:52:28 -0400

    removed inccorect implentaion

--- poker.c
+++ poker.c
@@ -150,49 +150,6 @@ void makeStraightFlush3(Hand hand){
 }
 
 
-
-// int main(int argc,char** argv){
-// 	int comm_sz;
-// 	int my_rank;
-// 	MPI_Init(&argc,&argv);
-// 	int straightFlushes=0;
-
-// 	MPI_Comm_size(MPI_COMM_WORLD,&comm_sz);
-// 	MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
-// 	float percent;
-// 	Hand pokerHand;
-// 	srand(time(0));
-// 	int cnt;
-// 	getTotalTrials(&cnt / comm_sz, my_rank);
-// 	for (int i=0;i<cnt;i++){
-// 		int cardCount=0;
-// 		while (cardCount<5){
-// 			Card card;
-// 			randomCard(&card);
-// 			if (!inHand(&card,pokerHand,cardCount)){
-// 				pokerHand[cardCount].rank=card.rank;
-// 				pokerHand[cardCount].suit=card.suit;
-// 				cardCount++;
-// 			}
-// 		}
-// #ifdef DEBUG
-// 		printHand(pokerHand);
-// #endif
-// 		if (isStraightFlush(pokerHand))
-// 			straightFlushes++;
-// 	}
-
-	
-// 	percent=(float)straightFlushes/(float)cnt*100.0;
-
-// 	if (my_rank == 0) {
-//     printf("We found %d straight flushes out of %d hands or %f percent.\n",
-//            straightFlushes,cnt,percent);
-// }
-// 	MPI_Finalize();
-// 	return 0;
-// }
-
 int main(int argc,char** argv){
 
     int comm_sz;
